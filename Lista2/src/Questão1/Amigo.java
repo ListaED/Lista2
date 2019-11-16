b3962be89bc1e93ea0abdb46e60782cfa39e9a03
JuanBailke <juanbailke1@gmail.com>
2019-11-16 02:14:53 -0300

    Início questão 1

---
+++ Lista2/src/Questão1/Amigo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questão1;
+
+/**
+ *
+ * @author Pichau
+ */
+public class Amigo {
+    
+    private String nome;
+    private int dia;
+    private int mes;
+    private int ano;
+    private String situacao;
+
+    //<editor-fold defaultstate="collapsed" desc="Getters & Setters">
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getDia() {
+        return dia;
+    }
+    
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    
+    public int getMes() {
+        return mes;
+    }
+    
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    
+    public int getAno() {
+        return ano;
+    }
+    
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    public String getSituacao() {
+        return situacao;
+    }
+    
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+//</editor-fold>
+    
+}
